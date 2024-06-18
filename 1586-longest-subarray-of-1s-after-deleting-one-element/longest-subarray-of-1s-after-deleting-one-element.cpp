7114bfd15bb7581fff7a0fe649fa658b6185535c
jekovic <97078354+jekovic@users.noreply.github.com>
2024-06-18 10:59:10 +0900

    Time: 51 ms (14.49%) | Memory: 58.7 MB (40.88%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,13 +3,9 @@ public:
     int longestSubarray(vector<int>& nums) {
         int result = 0;
         int left = 0, right = 0, z = -1;
-
-        while(right<nums.size())
+        int n = nums.size();
+        while(right<n)
         {
-            if(right == nums.size() and z == -1){
-                result = max(result, right - left);
-                break;
-            }
             if(nums[right] == 0 and z == -1){
                 z = right;
                 //right++;
@@ -17,6 +13,11 @@ public:
             else if(nums[right] == 0 and z != -1){
                 left = ++z;
                 z = right;
+            }
+
+            if(right == n and z == -1){
+                result = max(result, right - left);
+                break;
             }
             
             result = max(result, right - left);
