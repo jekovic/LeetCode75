e80fa0d1bf4f9dac29f4141a511629c478335831
jekovic <97078354+jekovic@users.noreply.github.com>
2024-07-01 16:17:55 +0900

    Time: 14 ms (59.47%) | Memory: 19.3 MB (29.79%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,26 +2,9 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        unordered_map<int, int> m;
-        unordered_map<int, int> um;
-        for(int n:nums) {
-            if(n >= 0)
-                m[n]++;
-            else {
-                int idx= abs(n);
-                um[idx]++;
-            }
-        }
-        for(int i=0; i != m.size(); i++) {
-            if(m[i] == 1){
-                result = i;
-            }
-        }
-        for(int i=0; i != um.size(); i++) {
-            if(um[i] == 1){
-                result = i*(-1);
-            }
-        }
+        for(int n : nums){
+            result ^= n;
+        }   
         return result;
     }
 };
