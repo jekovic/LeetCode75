42c3915a67c2e3fbdf04c03c4fdc2a19246a5119
jekovic <97078354+jekovic@users.noreply.github.com>
2024-07-16 11:28:51 +0900

    Time: 0 ms (100.00%) | Memory: 12 MB (12.28%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.cpp
@@ -0,0 +1,51 @@
+double bfs(string start, string end, map<string, vector<string>>& g, map<pair<string, string>, double>& mp) {
+    if(g[start].empty() || g[end].empty()) return -1.0;
+    if(start == end) return 1.0;
+    map<string, string> parent;
+    queue<string> q;
+    q.push(start);
+
+    while(!q.empty()) {
+        auto t = q.front();
+        q.pop();
+
+        if(t == end){
+            double res = 1.0;
+            while(t!=start){
+                res *= mp[{parent[t], t}];
+                t = parent[t];
+            }
+            return res;
+        }
+
+        for(auto x : g[t]) {
+            if(x != parent[t] and !parent[x].length()){
+                parent[x] = t;
+                q.push(x);
+            }
+        }
+    }
+    return -1.0;   
+}
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        vector<double> res;
+        map<string, vector<string>> g;
+        map<pair<string, string>, double> mp;
+
+        for(int i=0; i!= values.size(); i++) {
+            auto x = equations[i];
+            g[x[0]].push_back(x[1]);
+            g[x[1]].push_back(x[0]);
+            mp[{x[0], x[1]}] = values[i];
+            mp[{x[1], x[0]}] = 1.0/values[i];            
+        }
+        
+        for(auto x : queries) {
+            res.push_back(bfs(x[0], x[1], g, mp));
+        }
+
+        return res;
+    }
+};
