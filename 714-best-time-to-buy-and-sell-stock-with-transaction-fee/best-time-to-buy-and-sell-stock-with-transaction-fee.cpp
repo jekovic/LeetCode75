bc6045103853f814c4cb0a1a6a1cf41c8a4eac18
jekovic <97078354+jekovic@users.noreply.github.com>
2024-07-18 13:10:07 +0900

    Time: 63 ms (95.53%) | Memory: 57.6 MB (76.10%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int d(int i, int canbuy, vector<int>& prices, int fee) {
-        int n = prices.size();
-        if(i >=n) return 0;
-        if(dp[i][canbuy] != -1) return dp[i][canbuy];
-        if(canbuy) { // 1 -> stay or buy
-            dp[i][canbuy] = max(-prices[i]+d(i+1, 0, prices, fee), d(i+1, 1, prices, fee));
-        }
-        else{ //0 -> sell or stay
-            dp[i][canbuy] = max(prices[i]-fee+d(i+1, 1, prices, fee), d(i+1, 0, prices, fee));
-        }
-        return dp[i][canbuy];
-    }
-
     int maxProfit(vector<int>& prices, int fee) {
-        dp.assign(prices.size()+1, vector<int>(2, -1));    
-    
-        return d(0, 1, prices, fee);
+        int n = prices.size();
+        if(n == 0) return 0;
+
+        int curbuy, curnot, aheadbuy, aheadnot;
+        aheadbuy = 0;
+        aheadnot = 0;
+
+        for(int i=prices.size()-1; i>=0; i--) {
+            curbuy = max(-prices[i] + aheadnot, 0 + aheadbuy);
+            curnot = max(prices[i] - fee + aheadbuy, 0 + aheadnot);
+            aheadbuy = curbuy;
+            aheadnot = curnot;
+        }
+
+        return aheadbuy;
     }
 };
